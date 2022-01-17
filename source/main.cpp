f5b81c988c5d9ae8eb40e2f6ade14583aa1af3f7
LUÂN NGUYỄN CÔNG ANH <luan.nguyenexecutive@hcmut.edu.vn>
2022-01-17 18:05:24 +0000

    Replace main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,13 @@
+#include"mainThings.h"
 #undef main
 
-int main(int arc, char* argv[] )
-{
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = NULL;
-	SDL_Renderer* renderer = NULL;
-	window = SDL_CreateWindow("Nothing here",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,576,0);
-	renderer = SDL_CreateRenderer(window,0,SDL_RENDERER_ACCELERATED);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-	SDL_RenderClear(renderer);
-	SDL_Delay(5000);
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-	Mix_CloseAudio();
-	//cleans up all initialized subsystems
+int main(int arc, char* argv[] ){
+	if (!game::init()) return -1
+
+	
+	
+	//Clean up
+	game::destroy();
 	SDL_Quit();
 	return 0;
 }
