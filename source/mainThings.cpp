4b5044c54c545fbf3a188a086d778f89b8154bb9
LUÂN NGUYỄN CÔNG ANH <luan.nguyenexecutive@hcmut.edu.vn>
2022-01-18 02:48:58 +0000

    Replace mainThings.cpp

--- source/mainThings.cpp
+++ source/mainThings.cpp
@@ -1,48 +1,28 @@
 #include"mainThings.h"
-bool game::init(){
-    if (SDL_Init(SDL_INIT_EVERYTHING)!=0) return false;
-    window = SDL_CreateWindow(window,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,576,0);
-    if (window==NULL) return false;
-    renderer = SDL_CreateRenderer(window,-1,0);
-    if (renderer==NULL) return false;
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    anhNen=IMG_Load(".....");
-    if (anhNen==NULL) return false;
-    return true;
-}
-
-void game::destroy(){
-    SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-	Mix_CloseAudio();
-    SDL_FreeSurface(anhNen);
-    SDL_FreeSurface(tempAnhNen);
-}
-
 
 bool game::xoaNen(SDL_Surface* a){
     if (SDL_SetColorKey(a,SDL_TRUE,SDL_MapRGB(a->format,255,192,203))!=0) return false;
     return true;
-}
+};
 
 bool game::ganAnh(SDL_Surface* a, int x, int y) {
     SDL_Rect rect;
     rect.x=x;
     rect.y=y;
-    if (SDL_BlitSurface(a,NULL,tempAnhNen,rect)!=0) return false;
+    if (SDL_BlitSurface(a,NULL,tempAnhNen,&rect)!=0) return false;
     return true;
-}
+};
 
 SDL_Surface* game::loadAnh(std::string a) {
     SDL_Surface* result = NULL;
     result = IMG_Load(a.c_str());
     return result;
-}
+};
 
-bool game::taoAnhNen(){
+void game::taoAnhNen(){
     tempAnhNen->clip_rect=anhNen->clip_rect;
     tempAnhNen->flags = anhNen->flags;
-    tempAnhNen->format = anhNen->flags;
+    tempAnhNen->format = anhNen->format;
     tempAnhNen->h = anhNen->h;
     tempAnhNen->list_blitmap = anhNen->list_blitmap;
     tempAnhNen->locked = anhNen->locked;
@@ -50,6 +30,39 @@ bool game::taoAnhNen(){
     tempAnhNen->pitch = anhNen->pitch;
     tempAnhNen->pixels = anhNen->pixels;
     tempAnhNen->refcount = anhNen->refcount;
-    tempAnhNen->userdata = anhNen->refcount;
+    tempAnhNen->userdata = anhNen->userdata;
     tempAnhNen->w = anhNen->w;
+};
+
+void game::thaydoithuoctinh(char a, short b) {
+    switch (a) {
+        case 'M': 
+            Money+=b;
+            if (Money>=100) Winning1=true;
+            else if (Money<=0) Death=true;
+            break;
+        case 'F':
+            Favor+=b;
+            if (Favor>=100 || Favor<=0) Death=true;
+            break;
+        case 'O':
+            Oxygen+=b;
+            if (Oxygen<=0) Death=true;
+            else if (Oxygen>100) Oxygen=100;
+            break;
+        case 'I':
+            Influ+=b;
+            if (Influ>=100) Winning2=true;
+            else if (Influ<=0) Death=true;
+            break;
+        case 'T':
+            Trans+=b;
+            if (Trans>=100) Death=true;
+            else if (Trans<0) Trans=0;
+    }
+}
+
+void game::bienDoi(){
+    thaydoithuoctinh('O',-10);
+    thaydoithuoctinh('T',5);
 }
