be92d878148c8a5ca442e25edc251ed0045a99c1
LUÂN NGUYỄN CÔNG ANH <luan.nguyenexecutive@hcmut.edu.vn>
2022-01-18 08:59:45 +0000

    Replace mainThings.cpp

--- source/mainThings.cpp
+++ source/mainThings.cpp
@@ -9,7 +9,7 @@ bool game::ganAnh(SDL_Surface* a, int x, int y) {
     SDL_Rect rect;
     rect.x=x;
     rect.y=y;
-    if (SDL_BlitSurface(a,NULL,tempAnhNen,&rect)!=0) return false;
+    if (SDL_BlitSurface(a,NULL,anhNen,&rect)!=0) return false;
     return true;
 };
 
@@ -19,45 +19,31 @@ SDL_Surface* game::loadAnh(std::string a) {
     return result;
 };
 
-void game::taoAnhNen(){
-    tempAnhNen->clip_rect=anhNen->clip_rect;
-    tempAnhNen->flags = anhNen->flags;
-    tempAnhNen->format = anhNen->format;
-    tempAnhNen->h = anhNen->h;
-    tempAnhNen->list_blitmap = anhNen->list_blitmap;
-    tempAnhNen->locked = anhNen->locked;
-    tempAnhNen->map = anhNen->map;
-    tempAnhNen->pitch = anhNen->pitch;
-    tempAnhNen->pixels = anhNen->pixels;
-    tempAnhNen->refcount = anhNen->refcount;
-    tempAnhNen->userdata = anhNen->userdata;
-    tempAnhNen->w = anhNen->w;
-};
-
 void game::thaydoithuoctinh(char a, short b) {
     switch (a) {
         case 'M': 
             Money+=b;
             if (Money>=100) Winning1=true;
-            else if (Money<=0) Death=true;
+            else if (Money<=0) Death1=true;
             break;
         case 'F':
             Favor+=b;
-            if (Favor>=100 || Favor<=0) Death=true;
+            if (Favor<=0) Death2=true;
+            else if (Favor>=100) Winning3=true;
             break;
         case 'O':
             Oxygen+=b;
-            if (Oxygen<=0) Death=true;
+            if (Oxygen<=0) Death3=true;
             else if (Oxygen>100) Oxygen=100;
             break;
         case 'I':
             Influ+=b;
             if (Influ>=100) Winning2=true;
-            else if (Influ<=0) Death=true;
+            else if (Influ<=0) Death4=true;
             break;
         case 'T':
             Trans+=b;
-            if (Trans>=100) Death=true;
+            if (Trans>=100) Death5=true;
             else if (Trans<0) Trans=0;
     }
 }
