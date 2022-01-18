7acf1dac57041eec0317a3670cc5c8296c1ba3ab
LUÂN NGUYỄN CÔNG ANH <luan.nguyenexecutive@hcmut.edu.vn>
2022-01-18 14:53:20 +0000

    Replace mainThings.h

--- source/mainThings.h
+++ source/mainThings.h
@@ -4,8 +4,10 @@
 #include <SDL.h>
 #include <SDL_mixer.h>
 #include <SDL_image.h>
-#include <SDL_ttf.h>
 #include <string>
+#include <cstdlib>
+
+
 
 extern SDL_Window* window;
 extern SDL_Renderer* renderer;
@@ -13,29 +15,24 @@ extern SDL_Surface* anhNen;
 extern SDL_Texture* texture;
 extern Mix_Music* nhacNen;
 extern SDL_Event event;
-extern TTF_Font* font;
-extern SDL_Color mauchu;
+
 
 extern short Money;
 extern short Favor;
 extern short Oxygen;
 extern short Influ;
 extern short Trans;
-extern bool Death1;
-extern bool Death2;
-extern bool Death3;
-extern bool Death4;
-extern bool Death5;
-extern bool Winning1;
-extern bool Winning2;
-extern bool Winning3;
+extern short ketThuc;
+extern bool isRunning;
+extern short khungNen;
+extern short turn;
+
+
 
 namespace game {
-    bool xoaNen(SDL_Surface* a);
-    bool ganAnh(SDL_Surface* a, int x, int y);
-    SDL_Surface* loadAnh(std::string a);
     void thaydoithuoctinh(char a, short b);
     void bienDoi();
+    void showNen(int a);
 }
 
 #endif
